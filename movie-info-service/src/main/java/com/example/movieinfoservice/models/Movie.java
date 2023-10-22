7d1c5260927332d55d7d6050a5f174367ec8f27b
srss-pocs <145287517+srss-pocs@users.noreply.github.com>
2023-10-22 14:58:12 +0530

    Movie info service

---
+++ movie-info-service/src/main/java/com/example/movieinfoservice/models/Movie.java
@@ -0,0 +1,14 @@
+package com.example.movieinfoservice.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Movie {
+	private String movieId;
+    private String name;
+    private String description;
+}
---
+++ movie-info-service/src/main/java/com/example/movieinfoservice/models/MovieSummary.java
@@ -0,0 +1,15 @@
+package com.example.movieinfoservice.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MovieSummary {
+
+	private String id;
+    private String title;
+    private String overview;
+}
